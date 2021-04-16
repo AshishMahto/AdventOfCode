f03bd694f0d72707f7d037718401570c8100f766
Ashish <ashish.mahto123abc@gmail.com>
2021-04-16 11:05:04 -0400

    Day 1

--- src/main/scala/Advent2020/Day1/Day1.py
+++ src/main/scala/Advent2020/Day1/Day1.py
@@ -1,2 +1,6 @@
 
+f = set(map(int, open("input.txt").read().split()))
 
+print({i * (2020 - i) for i in f if 2020 - i in f })
+
+print({ i * j * (2020 - i - j) for i in f for j in f if 2020 - i - j in f})
