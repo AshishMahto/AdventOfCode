74e8ed2e49087ff375a9a0b53a37795c331e799d
Ashish <ashish.mahto123abc@gmail.com>
2021-04-16 14:39:33 -0400

    Day 7

--- src/main/scala/Advent2020/Day7/Day7.py
+++ src/main/scala/Advent2020/Day7/Day7.py
@@ -1,8 +1,43 @@
+import functools
 
-import requests
-import builtins
 
-def dl(i, cookie="53616c7465645f5f20e9261536f734605b8f8b979cea01779a9cd3dc55811cecf2604a9358df1bda489dfede22dc1461"):
-  r = requests.get(f'https://adventofcode.com/2020/day/{i}/input', cookies=dict(session=cookie))
-  with builtins.open(f"Day{i}/input.txt", 'wb') as f:
-    f.write(r.content)
+def remove_bags(s):
+  if ' bags' in s:
+    return s[:-5]
+  else:
+    return s[:-4]
+
+
+def toInt(s):
+  i, x = s.split(' ', 1)
+  return int(i), remove_bags(x)
+
+
+def parse1(_, x):
+  x = x.strip('.').split(', ')
+  return remove_bags(_), [toInt(c) for c in x if c != "no other bags"]
+
+
+f = dict(
+  parse1(*ln.split(' contain '))
+  for ln in open("input.txt").read().strip().split('\n')
+)
+
+
+@functools.cache
+def num_shiny_gold(node): return sum(
+  num if child == 'shiny gold' else num_shiny_gold(child)
+  for num, child in f[node]
+)
+
+
+@functools.cache
+def num_children(node): return sum(
+  num + num * num_children(child)
+  for num, child in f[node]
+)
+
+
+# print(*f.items(), sep="\n")
+print(sum([num_shiny_gold(nd) >= 1 for nd in f]))
+print(num_children('shiny gold'))
