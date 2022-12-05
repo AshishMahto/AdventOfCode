8f9abaf6886787b5bf9e5dbb1f48fca0510d146d
Ashish Mahto <ashish_mahto@homedepot.com>
2022-12-05 10:22:25 -0500

    Day 5, 2022

---
+++ Advent3/src/main/scala/Advent2022/D05.scala
@@ -0,0 +1,45 @@
+package Advent2022
+
+import Shared.D
+
+import scala.collection.mutable.ArrayBuffer
+
+private[this] object D05 extends D {
+//  override val input =
+  """[D]        
+    |[N] [C]    
+    |[Z] [M] [P]
+    | 1   2   3 
+    |
+    |move 1 from 2 to 1
+    |move 3 from 1 to 3
+    |move 2 from 2 to 1
+    |move 1 from 1 to 2""".stripMargin
+  
+  val Array(setup, rawMoves) = this.input.split("\n\n")
+  val len = 9
+  val crates =
+    val a = ArrayBuffer.fill(len)(List.empty[Char])
+    setup.linesIterator.toList.init.reverse foreach { s =>
+      0 until len foreach { i =>
+        if s(4 * i + 1) != ' ' then
+          a(i) ::= s(4 * i + 1)
+      }
+    }
+    a.toVector
+
+  val parseMove = raw"move (\d+) from (\d+) to (\d+)".r
+  val moves = rawMoves.split("\n").map { case parseMove(n, l, r) => (n.toInt, l.toInt - 1, r.toInt - 1) }
+
+  moves.foldLeft(crates) { case (a, (n, l, r)) =>
+    val (moved, rest_l) = a(l).splitAt(n)
+    a.updated(l, rest_l)
+     .updated(r, moved reverse_::: a(r))
+  }.map(_.head).mkString.part
+
+  moves.foldLeft(crates) { case (a, (n, l, r)) =>
+    val (moved, rest_l) = a(l).splitAt(n)
+    a.updated(l, rest_l)
+     .updated(r, moved ::: a(r))
+  }.map(_.head).mkString.part
+}
