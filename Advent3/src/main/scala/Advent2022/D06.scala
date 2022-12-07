b864f876b2833fa762bdc396dbddddc46793e229
Ashish Mahto <ashish_mahto@homedepot.com>
2022-12-06 19:14:19 -0500

    Day 6, 2022

--- Advent3/src/main/scala/Advent2022/D06.scala
+++ Advent3/src/main/scala/Advent2022/D06.scala
@@ -3,6 +3,7 @@ package Advent2022
 import Shared.D
 
 private[this] object D06 extends D {
-  Input.str.sliding(4).zipWithIndex.collectFirst { case (str, i) if str.toSet.sizeIs == 4 => i + 4 }.get.part
-  Input.str.sliding(14).zipWithIndex.collectFirst { case (str, i) if str.toSet.sizeIs == 14 => i + 14 }.get.part
+  def go(n: Int) = Input.str.sliding(n).zipWithIndex.collectFirst { case (str, i) if str.toSet.sizeIs == n => i + n }.get
+  go(4).part
+  go(14).part
 }
