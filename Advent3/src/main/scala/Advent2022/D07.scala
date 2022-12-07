4addc87e8a5c0e3bec2a2c6aa2e04e7c78170eb0
Ashish Mahto <ashish_mahto@homedepot.com>
2022-12-07 10:06:43 -0500

    Day 7, 2022

--- Advent3/src/main/scala/Advent2022/D07.scala
+++ Advent3/src/main/scala/Advent2022/D07.scala
@@ -3,30 +3,6 @@ package Advent2022
 import Shared.D
 
 private[this] object D07 extends D {
-  override protected val input = 
-  """$ cd /
-    |$ ls
-    |dir a
-    |14848514 b.txt
-    |8504156 c.dat
-    |dir d
-    |$ cd a
-    |$ ls
-    |dir e
-    |29116 f
-    |2557 g
-    |62596 h.lst
-    |$ cd e
-    |$ ls
-    |584 i
-    |$ cd ..
-    |$ cd ..
-    |$ cd d
-    |$ ls
-    |4060174 j
-    |8033020 d.log
-    |5626152 d.ext
-    |7214296 k""".stripMargin
 
   sealed trait Tree:
     val size: Long
